50f49655e697c574259d705871b296e5276a8bb4
7Priyanshu9 <priyanshupaul32@gmail.com>
2025-02-02 10:46:50 +0530

    About section updated

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Globe from 'react-globe.gl'
 import { temp } from 'three/webgpu'
+import Connect from '../components/Connect'
 
 const About = () => {
+
+    const[isCopied,setIsCopied]=useState(false);
+
+    const copyMail=()=>{
+        navigator.clipboard.writeText('Priyanshupaul32@gmail.com')
+        setIsCopied(true);
+        setTimeout(()=>{
+            setIsCopied(false)
+        },2000)
+    }
+
   return (
     <section className='c-space my-20'>
         <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-1 gap-5 h-full overflow-x-hidden'>
@@ -49,8 +61,39 @@ const About = () => {
                         
                         />
                     </div>
+                    <p className='grid-headtext'>I can work remotely acoss most time zones </p>
+                    <p className="grid-subtext">I'm from India, from kolkata</p>
+                    <Connect name ='Connect with me' isBeam={true} containerClass='bg-[#ff0066]'/>
                 </div>
             </div>
+
+
+            <div className="xl:col-span-2 xl:row-span-3" > 
+                            <div className="grid-container">
+                                <img src="/assets/leet.png" className='w-full object-contain ' alt="" />
+                                <p className='grid-headtext'>Problem Solving
+                                Passionate about algorithmic problem-solving. </p>
+                                <p className='grid-subtext'>My problem-solving journey reflects my proficiency in data structures, algorithms, and coding efficiency, helping me sharpen my logical thinking and optimize solutions. I continuously challenge myself with new problems, contests, and real-world coding scenarios to improve my skills.</p>
+                            </div>
+
+                        </div>
+
+            <div className="xl:col-span-1 xl:row-span-2">
+                <div className="grid-container">
+                    <img src="/assets/contactMe.jpg" alt="Contact me"  className='lg:w-full lg:h-full md:h-[126px] md:w-80 sm:h-[276px] sm:w-40   h-fit object-cover sm:object-fill '/>
+
+                    <div className="space-y-2">
+                        <p className="grid-subtext text-center"> Contact me </p>
+                        <div className='copy-container' onClick={copyMail}>
+                            <img src={isCopied ? 'assets/tick.svg' : 'assets/copy.svg' } alt="copy" />
+                            <p className='lg:text-2xl md:text-xl font-medium text-grey_gradient text-white '>PriyanshuPaul32@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                        
+
         </div>
     </section>
   )
