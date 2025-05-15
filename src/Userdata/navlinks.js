131babbca2f9db605deb795c33223f6606fb5292
PriyanshuPaul79 <priyanshupaul32@gmail.com>
2025-05-15 10:32:59 +0530

    added a new project

---
+++ src/Userdata/navlinks.js
@@ -0,0 +1,206 @@
+export const navLinks = [
+    {
+      id: 1,
+      name: 'Home',
+      href: '#home',
+    },
+    {
+      id: 2,
+      name: 'About',
+      href: '#about',
+    },
+    {
+      id: 3,
+      name: 'Work',
+      href: '#work',
+    },
+    {
+      id: 4,
+      name: 'Contact',
+      href: '#contact',
+    },
+  ];
+  
+  export const clientReviews = [
+    {
+      id: 1,
+      name: 'Sarah Patel',
+      position: 'Marketing Director at GreenLeaf',
+      img: 'assets/review1.png',
+      review:
+        'Priyanshu was an absolute pleasure to work with! He transformed our vision into a stunning, responsive, and high-performing website. His expertise in React, coupled with his attention to detail, made our platform both visually appealing and highly functional. Highly recommend him for any frontend development work!',
+    },
+    {
+      id: 2,
+      name: 'Mohit Jain ',
+      position: 'Founder of TechGear Shop',
+      img: 'assets/review2.png',
+      review:
+        'We hired Priyanshu to revamp our web application, and he exceeded our expectations! His ability to integrate complex frontend designs while ensuring a smooth user experience is truly impressive. His knowledge of both JavaScript and Python also helped us bridge the gap between frontend and backend seamlessly.',
+    },
+    {
+      id: 3,
+      name: 'John Dohsas',
+      position: 'Project Manager at UrbanTech ',
+      img: 'assets/review3.png',
+      review:
+        "Priyanshu is not just a frontend developer; he's a problem-solver. He took our outdated interface and modernized it with clean, intuitive designs while maintaining optimal performance. His professionalism and ability to collaborate made the entire process smooth and efficient. 100% recommended!",
+    },
+    {
+      id: 4,
+      name: 'Ether Smith',
+      position: 'CEO of BrightStar Enterprises',
+      img: 'assets/review4.png',
+      review:
+        "Working with Priyanshu was a fantastic experience. His grasp of frontend technologies, combined with his experience in Python projects, made him a valuable asset to our team. He delivered pixel-perfect designs, optimized for performance, and ensured smooth integrations with our backend. I wouldn't hesitate to work with him again!",
+    },
+  ];
+  
+  export const myProjects = [
+    {
+      title: 'RadioAtlas',
+      desc: 'Play retro radio songs at your fingertips.',
+      subdesc:
+        'With radioAtlas, users can listen to radio across different station across the world. It features a user-friendly interface, making it easy to find and enjoy various radio stations. Whether you want to relax, study, or just have a good time, radioAtlas has got you covered.',
+      href: 'https://radioatlas.streamlit.app',
+      texture: '/RadioAtlas.webm',
+      logo: '/assets/radio.svg',
+      logoStyle: {
+        backgroundColor: '#13202F',
+        border: '0.2px solid #17293E',
+        boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      },
+      spotlight: '/assets/spotlight2.png',
+      tags: [
+        {
+          id: 1,
+          name: 'Python',
+          path: '/assets/python.svg',
+        },
+        {
+          id: 2,
+          name: 'Streamlit',
+          path: 'assets/streamlit.png',
+        },
+      ],
+    },
+    {
+      title: 'AstroNautica',
+      desc: 'This website, titled "Travel," focuses on space tourism and stargazing.',
+      subdesc:
+        'Key features include a celestial events calendar, virtual space tours, sky maps and stargazing guides, and educational resources on astronomy and space exploration. The platform aims to enable users to experience the cosmos, whether through actual travel or virtual engagement.',
+      href: 'https://astro-nautica42.vercel.app',
+      texture: '/astronautica.webm',
+      logo: '/assets/space.png',
+      logoStyle: {
+        backgroundColor: '#60f5a1',
+        background:
+          'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+        border: '0.2px solid rgba(208, 213, 221, 1)',
+        boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+      },
+      spotlight: '/assets/spotlight3.png',
+      tags: [
+        {
+          id: 1,
+          name: 'Next.js',
+          path: '/assets/next.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+      ],
+    },
+    {
+      title: 'BrewHaven',
+      desc: 'Coffee Shop website',
+      subdesc:
+        'Built this for a freelance for a coffee vendor to sell and advertise the different type of coffee products.',
+      href:'https://brewhaven79.vercel.app/',
+      texture: '/brewHaven.webm',
+      logo: '/assets/brewhaven.png',
+      logoStyle: {
+        backgroundColor: '#2A1816',
+        border: '0.2px solid #36201D',
+        boxShadow: '0px 0px 60px 0px #AA3C304D',
+      },
+      spotlight: '/assets/spotlight1.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        
+      ],
+    },
+    {
+      title: 'NextHire',
+      desc: 'Website created to help everyone prepare for the upcoming Interviews.',
+      subdesc:
+        'Train today, triumph tomorrow In this website user can give mock interview with AI interviewer.Refine your technical, communication skills before the actual interview.',
+      href: 'https://github.com/PriyanshuPaul79/NextHire',
+      texture: '/NextHire.mp4',
+      logo: '/assets/NextHire.png',
+      logoStyle: {
+        backgroundColor: '#60f5a1',
+        background:
+          'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+        border: '0.2px solid rgba(208, 213, 221, 1)',
+        boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+      },
+      spotlight: '/assets/spotlight3.png',
+      tags: [
+        {
+          id: 1,
+          name: 'Next.js',
+          path: '/assets/next.svg',
+        },
+        {
+          id: 2,
+          name: 'Vapi',
+          path: 'assets/vapi.png',
+        },
+        {
+          id: 3,
+          name: 'Firebase',
+          path: '/assets/firebase.svg',
+        },
+        {
+          id: 4,
+          name: 'Typescript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id:5,
+          name:'Gemini',
+          path:'/assets/gemini.png'
+
+        }
+      ],
+    },
+  ]
+  
+  export const calculateSizes = (isSmall, isMobile, isTablet) => {
+    return {
+      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+      deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+      cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+      reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+      ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+      targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    };
+  };
+  
